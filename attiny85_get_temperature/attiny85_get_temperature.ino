16fc6018bf1a9f2ee69883dc023134e1353dbed1
MrCybernetic <cybernetic.msg@gmail.com>
2022-07-27 19:30:50 +0200

    Check VSCode setup for Arduino ✔

--- attiny85_get_temperature/attiny85_get_temperature.ino
+++ attiny85_get_temperature/attiny85_get_temperature.ino
@@ -1,7 +1,13 @@
-void setup() {
+#define LED_PIN 0
 
+void setup() {
+    pinMode(LED_PIN, OUTPUT);
 }
 
+// Dumb test to verify VSCode config -> ✔ Test successfull
 void loop() {
-
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
 }
