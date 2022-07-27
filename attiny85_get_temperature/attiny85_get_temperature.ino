b0a234c623b35076bb1667a5f62660a253ff1108
MrCybernetic <cybernetic.msg@gmail.com>
2022-07-27 22:03:40 +0200

    Temperature is displayed! 😁

--- attiny85_get_temperature/attiny85_get_temperature.ino
+++ attiny85_get_temperature/attiny85_get_temperature.ino
@@ -1,13 +1,21 @@
-#define LED_PIN 0
+#include <SoftwareSerial.h>
+#define comRx 3
+#define comTx 0
+
+SoftwareSerial soft_serial = SoftwareSerial(comRx, comTx);
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
+    soft_serial.begin(9600);
+    OSCCAL = 65;
+    uint8_t AD_mem = ADMUX;
+    ADMUX = 0b10101111 & (0b10001111 | AD_mem);// Set ADMUX to 0x8F in order to choose ADC4 and Set VREF to 1.1V (Without changing ADLAR p134 in datasheet)
 }
 
-// Dumb test to verify VSCode config -> ✔ Test successfull
 void loop() {
-    digitalWrite(LED_PIN, HIGH);
-    delay(500);
-    digitalWrite(LED_PIN, LOW);
-    delay(500);
+    ADCSRA = 0b01000000 | ADCSRA; // run conversion
+    while (bitRead(ADCSRA, 6) == 1) {
+        delayMicroseconds(10);
+    }
+    soft_serial.println((ADCH << 8) | ADCL);
+    delay(1000);
 }
